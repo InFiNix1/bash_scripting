fa98333c34c74ccf92805366a56b314ec49c5c4a
Sauronil Das <sauronildas10@gmail.com>
2020-03-21 12:38:26 +0530

    My Update Script

--- basics/update.sh
+++ basics/update.sh
@@ -10,8 +10,12 @@ date >> /home/sauronil/update.log
 
 
 #2 here shows error
-#sudo apt update && sudo apt upgrade -yy 2>> /home/sauronil/update.log
+sudo apt update && sudo apt upgrade -yy 2>> /home/sauronil/update.log
 
 
 echo "Installing Updates ...."
+
+echo "Update Completed On: " >> /home/sauronil/update.log
+
+date>> /home/sauronil/update.log
 
