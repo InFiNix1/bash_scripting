44a56c58a1656dd2afcf18cbee8aad5e0ccc29a6
Sauronil Das <sauronildas10@gmail.com>
2020-05-26 16:41:58 +0530

    Shell Something Out

--- udemy/01_Shell-Something-Out/colored_text.sh
+++ udemy/01_Shell-Something-Out/colored_text.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo -e "\e[13;31mThis is A Red Text\e[0m"
+echo -e "\e[1;31mThis is A Red Text\e[0m"
+
+#Multiple Commands by using ; separation
+
+echo -e "\e[1;5;31;43mThere is No True Despair Without Hope\e[0m"
